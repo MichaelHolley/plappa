8a00d5c38b788efbac4434e13fe0f315c73f08b6
Michael Holley <michael.philipp.holley@gmail.com>
2026-04-18 08:11:34 +0200

    feat: persist in local-storage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,19 @@
 	import type { Message as PromptMessage } from '$lib/components/ai-elements/prompt-input';
 	import * as PromptInput from '$lib/components/ai-elements/prompt-input';
 	import { Chat } from '@ai-sdk/svelte';
+	import { browser } from '$app/environment';
 
-	const chat = new Chat({});
+	const CHAT_ID = 'plappa-main-chat';
+
+	const savedMessages = browser ? JSON.parse(localStorage.getItem(CHAT_ID) ?? 'null') : null;
+
+	const chat = new Chat({
+		id: CHAT_ID,
+		messages: savedMessages ?? [],
+		onFinish() {
+			localStorage.setItem(CHAT_ID, JSON.stringify(chat.messages));
+		}
+	});
 
 	function handleSubmit(message: PromptMessage) {
 		chat.sendMessage({ text: message.text });
@@ -12,7 +23,7 @@
 </script>
 
 <main class="flex h-full flex-col gap-1 p-2">
-	<div class="mt-4 h-full w-full overflow-y-auto">
+	<div class="mt-4 h-full w-full overflow-y-auto pr-4">
 		<ul>
 			{#each chat.messages as message, messageIndex (messageIndex)}
 				<Message from={message.role} class="my-4">
